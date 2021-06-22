2c546aceef7a5c4621376fdcdc42b733f8f7d2fe
Ursula Wilborn <wilbornu@yahoo.com>
2021-06-22 01:16:38 -0400

    updated

--- Assets/script.js
+++ Assets/script.js
@@ -1,23 +1,25 @@
 var APIkey = "f3bc0a3c85231ad135e4beb406f8cdc7";
-let city;
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey;
 
 let userinput = document.getElementById("userinput");
 const submit = document.getElementById("submit");
 
 // function containing variable that stores input from the user
 function grabText(){
-    city = userinput.value;
-    alert(city);
+    let city = userinput.value;
+   
     localStorage.setItem("userinput", JSON.stringify(city));
     console.log(city);
 }
 
 // function 
-function getApi(queryURL) {
-    fetch("https//api.openweathermap.org/data/2.5/forecast?id={city ID}&appid={API key}")
-      console.log(queryURL)
-    }
+function getApi(city,APIkey) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey;
+    fetch(queryURL).then(function (response) {
+                return response.json();
+          })
+          .then(function (data) {
+            console.log(data);
+          })};
   
   getApi();
 
