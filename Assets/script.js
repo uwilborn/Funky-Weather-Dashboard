1683844b07fa0a5b3e9f2414a5726a2d280357eb
Ursula Wilborn <wilbornu@yahoo.com>
2021-07-05 12:45:13 -0400

    Fixed the formatting of the forecast blocks

--- Assets/script.js
+++ Assets/script.js
@@ -13,7 +13,13 @@ const submit = document.getElementById("submit");
 
 
 function grabText(event){
-  event.preventDefault();  
+  event.preventDefault(); 
+  // currentWeather.innerText = "";
+  // currentWeather2.innerText = ""; 
+  // currentWeather3.innerText = ""; 
+  // currentWeather4.innerText = ""; 
+  // currentWeather5.innerText = ""; 
+  // currentWeather6.innerText = "";  
   let city = userinput.value;
   getApi(city);
     localStorage.setItem("userinput", JSON.stringify(city));
@@ -42,11 +48,14 @@ currentTemp.innerText = fTemp.toFixed(0) + "\xB0" + "F";
 
 currentWeather.append(currentTemp);
 
+
+
 get5Day(data.coord.lat,data.coord.lon);
 }
 
 function get5Day(lat,lon){
   var queryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${APIkey}`
+  console.log(queryURL);
   fetch(queryURL)
   .then(response => response.json())
   .then(data => display5Day(data));
@@ -86,15 +95,18 @@ function get5Day(lat,lon){
 function display5Day(data){
   
   let fdate = document.createElement("h3");
-  fdate.innerText = data.daily[0].dt;
+  fdate.innerText = convertTimestamp(data.daily[0].dt);
+  let ficon = document.createElement("div");
+  ficon.innerText = data.daily[0].weather;
   let futureTemp = document.createElement("h2");
   let convertTemp = ((data.daily[0].temp.day -273.15)*1.8)+32;
-  futureTemp.innerText = convertTemp.toFixed(0) + "\xB0" + "F";
+  futureTemp.innerText = "Temp: " + convertTemp.toFixed(0) + "\xB0" + "F";
   let fwind = document.createElement("h2");
-  fwind.innerText = data.daily[0].wind_speed;
+  fwind.innerText = "Wind: " + data.daily[0].wind_speed + " MPH";
   let fhumid = document.createElement("h2");
-  fhumid.innerText = data.daily[0].humidity;
+  fhumid.innerText = "Humidity: " + data.daily[0].humidity + " %";
   currentWeather2.append(fdate);
+  currentWeather2.append(ficon);
   currentWeather2.append(futureTemp);
   currentWeather2.append(fwind);
   currentWeather2.append(fhumid);
@@ -104,14 +116,14 @@ function display5Day(data){
   function display5Day2(data){
   
     let fdate = document.createElement("h3");
-    fdate.innerText = data.daily[1].dt;
+    fdate.innerText = convertTimestamp(data.daily[1].dt);
     let futureTemp = document.createElement("h2");
     let convertTemp = ((data.daily[1].temp.day -273.15)*1.8)+32;
-    futureTemp.innerText = convertTemp.toFixed(0) + "\xB0" + "F";
+    futureTemp.innerText = "Temp: " + convertTemp.toFixed(0) + "\xB0" + "F";
     let fwind = document.createElement("h2");
-    fwind.innerText = data.daily[1].wind_speed;
+    fwind.innerText = "Wind: " + data.daily[1].wind_speed + " MPH";
     let fhumid = document.createElement("h2");
-    fhumid.innerText = data.daily[1].humidity;
+    fhumid.innerText = "Humidity: " + data.daily[1].humidity + " %";
     currentWeather3.append(fdate);
     currentWeather3.append(futureTemp);
     currentWeather3.append(fwind);
@@ -122,14 +134,14 @@ function display5Day(data){
     function display5Day3(data){
   
       let fdate = document.createElement("h3");
-      fdate.innerText = data.daily[2].dt;
+      fdate.innerText = convertTimestamp(data.daily[2].dt);
       let futureTemp = document.createElement("h2");
       let convertTemp = ((data.daily[2].temp.day -273.15)*1.8)+32;
-      futureTemp.innerText = convertTemp.toFixed(0) + "\xB0" + "F";
+      futureTemp.innerText = "Temp: " + convertTemp.toFixed(0) + "\xB0" + "F";
       let fwind = document.createElement("h2");
-      fwind.innerText = data.daily[2].wind_speed;
+      fwind.innerText = "Wind: " + data.daily[2].wind_speed + " MPH";
       let fhumid = document.createElement("h2");
-      fhumid.innerText = data.daily[2].humidity;
+      fhumid.innerText = "Humidity: " + data.daily[2].humidity + " %";
       currentWeather4.append(fdate);
       currentWeather4.append(futureTemp);
       currentWeather4.append(fwind);
@@ -139,14 +151,14 @@ function display5Day(data){
       function display5Day4(data){
   
         let fdate = document.createElement("h3");
-        fdate.innerText = data.daily[3].dt;
+        fdate.innerText = convertTimestamp(data.daily[3].dt);
         let futureTemp = document.createElement("h2");
         let convertTemp = ((data.daily[3].temp.day -273.15)*1.8)+32;
-        futureTemp.innerText = convertTemp.toFixed(0) + "\xB0" + "F";
+        futureTemp.innerText = "Temp: " + convertTemp.toFixed(0) + "\xB0" + "F";
         let fwind = document.createElement("h2");
-        fwind.innerText = data.daily[3].wind_speed;
+        fwind.innerText = "Wind: " + data.daily[3].wind_speed + " MPH";
         let fhumid = document.createElement("h2");
-        fhumid.innerText = data.daily[3].humidity;
+        fhumid.innerText = "Humidity: " + data.daily[3].humidity + " %";
         currentWeather5.append(fdate);
         currentWeather5.append(futureTemp);
         currentWeather5.append(fwind);
@@ -157,19 +169,47 @@ function display5Day(data){
         function display5Day5(data){
   
           let fdate = document.createElement("h3");
-          fdate.innerText = data.daily[4].dt;
+          fdate.innerText = convertTimestamp(data.daily[4].dt);
           let futureTemp = document.createElement("h2");
           let convertTemp = ((data.daily[4].temp.day -273.15)*1.8)+32;
-          futureTemp.innerText = convertTemp.toFixed(0) + "\xB0" + "F";
+          futureTemp.innerText = "Temp: " + convertTemp.toFixed(0) + "\xB0" + "F";
           let fwind = document.createElement("h2");
-          fwind.innerText = data.daily[4].wind_speed;
-          let fhumid = document.createElement("h2");
-          fhumid.innerText = data.daily[4].humidity;
+          fwind.innerText = "Wind: " + data.daily[4].wind_speed + " MPH";
+          let fhumid = "Humidity: " + document.createElement("h2");
+          fhumid.innerText = "Humidity: " + data.daily[4].humidity + " %";
           currentWeather6.append(fdate);
           currentWeather6.append(futureTemp);
           currentWeather6.append(fwind);
           currentWeather6.append(fhumid);
           }
+
+
+          //Timestamp to Date Converter
+function convertTimestamp(timestamp) {
+  var d = new Date(timestamp * 1000), // Convert the passed timestamp to milliseconds
+      yyyy = d.getFullYear(),
+      mm = ('0' + (d.getMonth() + 1)).slice(-2),  // Months are zero based. Add leading 0.
+      dd = ('0' + d.getDate()).slice(-2),         // Add leading 0.
+      hh = d.getHours(),
+      h = hh,
+      min = ('0' + d.getMinutes()).slice(-2),     // Add leading 0.
+      ampm = 'AM',
+      time;
+
+  if (hh > 12) {
+      h = hh - 12;
+      ampm = 'PM';
+  } else if (hh === 12) {
+      h = 12;
+      ampm = 'PM';
+  } else if (hh == 0) {
+      h = 12;
+  }
+
+  // ie: 2014-03-24, 3:00 PM
+  time = mm + '/' + dd + '/' + yyyy;
+  return time;
+}
 
 submit.addEventListener("click", grabText)
 
